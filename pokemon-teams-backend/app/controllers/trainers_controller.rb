262915bc3e8870e2957723b878cd06614e3cc25a
pmhofmann <pm.hofmann@yahoo.co.uk>
2019-05-17 14:14:25 +0100

    trainers api built

--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -1,2 +1,20 @@
 class TrainersController < ApplicationController
+
+    def index 
+       @trainers = Trainer.all
+       render json: @trainers.to_json( 
+           include: {
+               pokemons: {
+                   except: [:created_at, :updated_at]
+                }
+            },
+            except: [:created_at, :updated_at]
+            )
+    end
+
+
+
+
+
+
 end
